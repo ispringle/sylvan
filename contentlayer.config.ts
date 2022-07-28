3f9eee776a013eac7e21c778a23ba44b6f597d80
Ian Pringle <ian@hydrobuilder.com>
2022-07-28 12:12:42 -0500

    Added rehype and remark plugins

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,9 +1,30 @@
 import { makeSource } from 'contentlayer/source-files';
 import { BookReview, BlogPost } from './contentlayer_type';
 
+import remarkGfm from 'remark-gfm'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import rehypePrism from 'rehype-prism-plus'
+import rehypeSlug from 'rehype-slug'
+
 const contentLayerConfig = makeSource({
   contentDirPath: 'content',
   documentTypes: [BookReview, BlogPost],
+  mdx: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      rehypeSlug,
+      rehypePrism,
+      [
+      rehypeAutolinkHeadings,
+      {
+          behavior: 'append',
+          properties: {
+            className: ['anchor']
+          },
+        },
+      ],
+    ],
+  },
 });
 
 export default contentLayerConfig;
