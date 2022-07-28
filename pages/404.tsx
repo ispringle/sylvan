2365a039359ce6ac9d8cd205cbdbbb3f375f870c
Ian Pringle <ian@hydrobuilder.com>
2022-07-28 15:24:46 -0500

    Best 404 page ever

---
+++ pages/404.tsx
@@ -0,0 +1,17 @@
+export default function Custom404() {
+  return (
+    <main>
+    <pre id="e404">
+      <div>             404             </div>
+      <div> </div>
+      <div> </div>
+      <div>      you are not where      </div>
+      <div>you ought to be. you are lost</div>
+      <div>      as best I can see      </div>
+      <div> </div>
+      <div> </div>
+      <div>               by Ian Pringle</div>
+    </pre>
+    </main>
+  )
+}
