7b27ecbc7413f061ed0e3b27ec392f5b7c7e3df9
Ian Pringle <ian@hydrobuilder.com>
2022-07-29 07:22:48 -0500

    Added missing values to About to appease typing

--- contentlayer_type/About.ts
+++ contentlayer_type/About.ts
@@ -6,6 +6,16 @@ export const About = defineDocumentType(() => ({
   filePathPattern: `about/**/*.mdx`,
   contentType: 'mdx',
   fields: {
+    title: {
+      type: 'string',
+      description: 'Title of content',
+      required: false,
+    },
+    date: {
+      type: 'string',
+      description: 'Date of content',
+      required: false,
+    },
     picture: {
       type: 'string',
       description: 'Picture of me',
