1c4015dc5acaad27926d9ea2fea83ab6546f8259
Ian Pringle <ian@hydrobuilder.com>
2022-08-06 14:18:31 -0500

    updated styling for codeblocks

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,11 +1,7 @@
-import * as path from 'path';
-import { trough } from 'trough';
-import { toVFile } from 'to-vfile';
-import { findDown } from 'vfile-find-down';
 import report from 'vfile-reporter';
+import * as path from 'path';
 
-import orgToHtml from './orgToHtml';
-import resolveLinks from './resolveLinks';
+import { processor } from './pipeline';
 
 // We serve posts from "public" directory, so that we don't have to
 // copy assets.
@@ -16,72 +12,6 @@ import resolveLinks from './resolveLinks';
 // const pagesDirectory = path.join(process.cwd(), 'public');
 const pagesDirectory = path.join(process.cwd(), 'src/content');
 const blackListedPaths = ["todo.org", "inbox.org",]
-
-const processor = trough()
-    .use(collectFiles)
-    .use(fileToAst)
-    .use(resolveLinks)
-    .use(populateBacklinks);
-
-function collectFiles(root) {
-    return new Promise((resolve, reject) => {
-        findDown(
-            (f, stats) => stats.isFile() && f.basename.endsWith('.org'),
-            root,
-            (err, files) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    files.forEach((f) => {
-                        f.filepath = f.path
-                        const slug =
-                            '/' + path.relative(root, f.path).replace(/\.org$/, '');
-                        f.data.slug = slug;
-                    });
-                    resolve(files);
-                }
-            }
-        );
-    });
-}
-
-async function fileToAst(files) {
-    return Promise.all(files.map(getAst));
-
-    async function getAst(file) {
-        try {
-            await toVFile.read(file, 'utf8');
-        } catch (e) {
-            console.error('Error reading file', file, e);
-            throw e;
-        }
-        file.path = file.data.slug;
-        if (file.filepath.endsWith('.org')) {
-            await orgToHtml(file);
-        } else {
-            throw new Error(`Unknown page type: $file.filepath`)
-        }
-
-        return file;
-    }
-}
-
-// Assign all collected backlinks to file. This function should be
-// called after all pages have been processed---otherwise, it might
-// miss backlinks.
-function populateBacklinks(files) {
-    const backlinks = {};
-    files.forEach((file) => {
-        file.data.links = file.data.links || new Set();
-        file.data.backlinks = backlinks[file.data.slug] =
-            backlinks[file.data.slug] || new Set();
-
-        file.data.links.forEach((other) => {
-            backlinks[other] = backlinks[other] || new Set();
-            backlinks[other].add(file.data.slug);
-        });
-    });
-}
 
 const loadPosts = async () => {
     const files = await new Promise((resolve, reject) =>
