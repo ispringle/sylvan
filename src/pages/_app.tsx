0980f4f2015840125ff2b29d2e038d0d8be15784
Ian Pringle <ian@hydrobuilder.com>
2022-08-17 12:26:30 -0500

    use fleuron for favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <>
             <Head>
                 <title>ian.ist</title>
+                <link rel="icon" href="8bit-fleuron.svg" />
             </Head>
             <DefaultSeo {...SEO} />
             <Header />
