3fd4c244a525a2c818e2d276c8bf490c173d6820
Ian Pringle <ian@hydrobuilder.com>
2022-09-08 10:29:53 -0500

    linting

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
-const packageJson = require('./package.json')
+const packageJson = require("./package.json");
 
-const nextBuildId = require('next-build-id')
+const nextBuildId = require("next-build-id");
 
 module.exports = {
   compiler: {
     removeConsole: true,
   },
   env: {
-    NEXT_PUBLIC_NEXT_JS_VERSION: packageJson.dependencies.next.replace('^', ''),
+    NEXT_PUBLIC_NEXT_JS_VERSION: packageJson.dependencies.next.replace("^", ""),
     NEXT_PUBLIC_BUILD_TIME: new Date(),
   },
   experimental: {
