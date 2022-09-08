3d7627bfc9fd01faf4303c0e64f9c47e9929ee51
Ian Pringle <ian@hydrobuilder.com>
2022-09-08 17:18:32 -0500

    Feeling cute, added a retro clock cursor...
    might delete later, idk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,30 @@
-import type { AppProps } from 'next/app'
-import Head from 'next/head'
-import { DefaultSeo } from 'next-seo';
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { DefaultSeo } from "next-seo";
+import { useEffect } from "react";
 
-import '../style/style.sass'
-import SEO from '../../next-seo.config';
-import Header from '../components/Header'
+import { clockCursor } from "cursor-effects";
+
+import "../style/style.sass";
+import SEO from "../../next-seo.config";
+import Header from "../components/Header";
+import { cursorTo } from "readline";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-    return (
-        <>
-            <Head>
-                <title>ian.ist</title>
-                <link rel="icon" href="fleuron.svg" />
-            </Head>
-            <DefaultSeo {...SEO} />
-            <Header />
+  useEffect(() => {
+    const targetEl = document.getElementsByTagName("main")[0];
+    clockCursor();
+  });
+  return (
+    <>
+      <Head>
+        <title>ian.ist</title>
+        <link rel="icon" href="fleuron.svg" />
+      </Head>
+      <DefaultSeo {...SEO} />
+      <Header />
 
-            <Component {...pageProps} />
-        </>
-    )
+      <Component {...pageProps} />
+    </>
+  );
 }
