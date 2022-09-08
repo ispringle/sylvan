9e696c66f05c114965564702451cf5b29a702e79
Ian Pringle <ian@hydrobuilder.com>
2022-09-08 08:35:12 -0500

    Removed span around linkList div

--- src/components/PropertiesDrawer.tsx
+++ src/components/PropertiesDrawer.tsx
@@ -1,48 +1,63 @@
-import OrgTime from './OrgTime'
+import OrgTime from "./OrgTime";
 
 export interface PropertiesDrawerProps {
-    properties: { [key: string]: string };
+  properties: { [key: string]: string };
 }
 
 const PropertiesDrawer = ({ ...props }) => {
-
-    const properties = props.properties
-    delete properties["author"]
-    return (
-        <div title="properties" className="psuedoDrawer">
-            <div className="drawerRow">
-                <span className="key">author</span>
-                <span>Ian Shepard Pringle</span>
-            </div>
-            {
-                Object.keys(properties).map(k => {
-                    return (
-                        <div key={k} className="drawerRow">
-                            <span className="key">{k}</span>
-                            <span>{properties[k]}</span>
-                        </div>
-                    )
-                })
-            }
-            <div className="drawerRow">
-                <span className="key">compiled</span>
-                <span>
-                    <OrgTime dateStr={process.env.NEXT_PUBLIC_BUILD_TIME} />
-                </span>
-            </div>
-            <div className="drawerRow">
-                <span className="key">made_with</span>
-                <span>
-                    <div className="linkList">
-                        <a rel="noreferrer" target="_blank" title="Emacs 28.1" href="https://www.gnu.org/software/emacs/">Emacs</a>
-                        <a rel=" noreferrer" target="_blank" title="Orgmode 9.5.2" href="https://orgmode.org">Orgmode</a>
-                        <a rel="noreferrer" target="_blank" title={"NextJS " + process.env.NEXT_PUBLIC_NEXT_JS_VERSION} href="https://nextjs.org">NextJS</a>
-                    </div>
-                </span>
-            </div>
+  const properties = props.properties;
+  delete properties["author"];
+  return (
+    <div title="properties" className="psuedoDrawer">
+      <div className="drawerRow">
+        <span className="key">author</span>
+        <span>Ian Shepard Pringle</span>
+      </div>
+      {Object.keys(properties).map((k) => {
+        return (
+          <div key={k} className="drawerRow">
+            <span className="key">{k}</span>
+            <span>{properties[k]}</span>
+          </div>
+        );
+      })}
+      <div className="drawerRow">
+        <span className="key">compiled</span>
+        <span>
+          <OrgTime dateStr={process.env.NEXT_PUBLIC_BUILD_TIME} />
+        </span>
+      </div>
+      <div className="drawerRow">
+        <span className="key">made_with</span>
+        <div className="linkList">
+          <a
+            rel="noreferrer"
+            target="_blank"
+            title="Emacs 28.1"
+            href="https://www.gnu.org/software/emacs/"
+          >
+            Emacs
+          </a>
+          <a
+            rel=" noreferrer"
+            target="_blank"
+            title="Orgmode 9.5.2"
+            href="https://orgmode.org"
+          >
+            Orgmode
+          </a>
+          <a
+            rel="noreferrer"
+            target="_blank"
+            title={"NextJS " + process.env.NEXT_PUBLIC_NEXT_JS_VERSION}
+            href="https://nextjs.org"
+          >
+            NextJS
+          </a>
         </div>
+      </div>
+    </div>
+  );
+};
 
-    )
-}
-
-export default PropertiesDrawer
+export default PropertiesDrawer;
