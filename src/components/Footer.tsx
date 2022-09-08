71d350ca2b3cf342aa24b6a240b5f8afa1c6ff26
Ian Pringle <ian@hydrobuilder.com>
2022-09-08 08:33:31 -0500

    Updated header and footer to be w3 compliant

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,29 +1,29 @@
-import Link from 'next/link'
-import Image from 'next/image'
+import Link from "next/link";
 
-import PropertiesDrawer from './PropertiesDrawer'
-import triquerta from '../../public/triquerta.svg'
+import PropertiesDrawer from "./PropertiesDrawer";
 
 const Footer = ({ ...props }) => {
-    return (
-        <footer>
-            <div id="footerLeft" className='footerSection'>
-                <PropertiesDrawer {...props} />
-            </div>
-            <div id="footerMid" className='footerSection'>
-                {/* <Image className='flueron' src={triquerta} width={"50em"} height={"50em"} /> */}
-                <img className='flueron' src={"fleuron.svg"} />
-            </div>
-            <div id="footerRight" className='footerSection'>
-                <div className="license">
-                    <p>Creative Commons</p>
-                    <Link rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
-                        <a>BY-NC-SA</a>
-                    </Link>
-                </div>
-            </div>
-        </footer >
-    )
-}
+  return (
+    <footer>
+      <div id="footerLeft" className="footerSection">
+        <PropertiesDrawer {...props} />
+      </div>
+      <div id="footerMid" className="footerSection">
+        <img className="flueron" src={"fleuron.svg"} alt="" />
+      </div>
+      <div id="footerRight" className="footerSection">
+        <div className="license">
+          <p>Creative Commons</p>
+          <Link
+            rel="license"
+            href="http://creativecommons.org/licenses/by-nc-sa/4.0/"
+          >
+            <a>BY-NC-SA</a>
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,37 +1,52 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 const Header = () => {
-    return (
-        <header>
-            <Link href="/"><a className="branding">
-                <pre id="logo"> _               _     _</pre>
-                <pre id="logo">(_)             (_)   | |</pre>
-                <pre id="logo"> _  __ _ _ __    _ ___| |_</pre>
-                <pre id="logo">| |/ _` | '_ \  | / __| __|</pre>
-                <pre id="logo">| | (_| | | | |_| \__ \ |_</pre>
-                <pre id="logo">|_|\__,_|_| |_(_)_|___/\__|</pre>
-            </a></Link>
-            <nav>
-                <ul>
-                    <li>
-                        <Link href="/about"><a>About</a></Link>
-                    </li>
-                    <li>
-                        <Link href="/blog"><a>Blog</a></Link>
-                    </li>
-                    <li>
-                        <Link href="/book"><a>Books</a></Link>
-                    </li>
-                    <li>
-                        <Link href="/loci"><a>Loci</a></Link>
-                    </li>
-                    <li>
-                        <Link href="/literate"><a>Literate</a></Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    )
-}
+  return (
+    <header>
+      <Link href="/">
+        <a className="branding">
+          {/* prettier-ignore */}
+          <div id="logo">
+            <pre className="logo"> _               _     _</pre>
+            <pre className="logo">(_)             (_)   | |</pre>
+            <pre className="logo"> _  __ _ _ __    _ ___| |_</pre>
+            <pre className="logo">| |/ _` | '_ \  | / __| __|</pre>
+            <pre className="logo">| | (_| | | | |_| \__ \ |_</pre>
+            <pre className="logo">|_|\__,_|_| |_(_)_|___/\__|</pre>
+          </div>
+        </a>
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/about">
+              <a>About</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/blog">
+              <a>Blog</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/book">
+              <a>Books</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/loci">
+              <a>Loci</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/literate">
+              <a>Literate</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
