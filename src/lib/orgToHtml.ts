0aef4e7b6230dfe0a18483add63bb829f4d7c2b9
Ian Pringle <ian@hydrobuilder.com>
2022-09-15 05:50:42 -0500

    Now supports rendering HTML in org docs
    Also had to comment out the import prepend for sass because it breaks
    the styles.sass import

--- src/lib/orgToHtml.ts
+++ src/lib/orgToHtml.ts
@@ -1,31 +1,33 @@
 // @ts-nocheck
 // TODO Fix the typing here re: uniorg-slug
-import { unified } from 'unified';
+import { unified } from "unified";
 
-import { NodeProperty } from 'uniorg';
-import orgParse from 'uniorg-parse';
-import org2rehype from 'uniorg-rehype';
-import extractKeywords from 'uniorg-extract-keywords';
-import { uniorgSlug } from 'uniorg-slug';
-import { visit } from 'unist-util-visit';
-import { visitIds } from 'orgast-util-visit-ids';
-import { uniorgAttach } from 'uniorg-attach';
+import { NodeProperty } from "uniorg";
+import orgParse from "uniorg-parse";
+import org2rehype from "uniorg-rehype";
+import extractKeywords from "uniorg-extract-keywords";
+import { uniorgSlug } from "uniorg-slug";
+import { visit } from "unist-util-visit";
+import { visitIds } from "orgast-util-visit-ids";
+import { uniorgAttach } from "uniorg-attach";
+import raw from "rehype-raw";
 
 const processor = unified()
   .use(orgParse)
   .use(extractKeywords)
   .use(extractProperties)
   .use(uniorgSlug)
-  .use(uniorgAttach, { idDir: '/.attach' })
+  .use(uniorgAttach, { idDir: "/.attach" })
   .use(extractIds)
   .use(org2rehype)
+  .use(raw)
   .use(toJson);
 
 export default async function orgToHtml(file) {
   try {
     return await processor.process(file);
   } catch (e) {
-    console.error('failed to process file', file.path, e);
+    console.error("failed to process file", file.path, e);
     throw e;
   }
 }
@@ -35,14 +37,14 @@ function extractProperties() {
 
   function transformer(tree: any, file: any) {
     const data = file.data || (file.data = {});
-    data.properties = {}
+    data.properties = {};
 
     // const props = tree.children[0]?.children?.[0];
     // console.log(tree.filter(prop => prop.type == "property-drawer"))
     // const props = tree.children.filter(child => child.type == 'property-drawer')?.[0]
     // console.log(props)
     // props?.children.forEach(node => data.properties[node.key] = node.value)
-    visit(tree, 'node-property', (prop: NodeProperty) => {
+    visit(tree, "node-property", (prop: NodeProperty) => {
       data.properties[prop.key.toLowerCase()] = prop.value;
     });
   }
@@ -57,9 +59,9 @@ function extractIds() {
     const ids = data.ids || (data.ids = {});
 
     visitIds(tree, (id, node) => {
-      if (node.type === 'org-data') {
-        ids[id] = '';
-      } else if (node.type === 'section') {
+      if (node.type === "org-data") {
+        ids[id] = "";
+      } else if (node.type === "section") {
         const headline = node.children[0];
         if (!headline.data?.hProperties?.id) {
           // The headline doesn't have an html id assigned. (Did you
@@ -71,7 +73,7 @@ function extractIds() {
           headline.data.hProperties.id = id;
         }
 
-        ids[id] = '#' + headline.data.hProperties.id;
+        ids[id] = "#" + headline.data.hProperties.id;
       }
     });
   }
