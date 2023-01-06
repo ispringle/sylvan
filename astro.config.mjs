0ee766ce3c5b664b029738bf7ddfa20fe8d29f6f
Ian S. Pringle <ipringle@jackhenry.com>
2023-01-06 11:10:42 -0600

    index cars added

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,19 @@ import remarkGfm from "remark-gfm";
 import remarkSmartypants from "remark-smartypants";
 import remarkObsidian from "./src/utils/remark-obsidian-md";
 import sectionize from "remark-sectionize";
+import critters from "astro-critters";
+// import partytown from "@astrojs/partytown";
+import purgecss from "astro-purgecss";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://ian.ist",
   trailingSlash: "always",
+  integrations: [
+    critters(),
+    // partytown(), // Not working with turbo
+    // This has to be last
+    purgecss(),
+  ],
   markdown: {
     remarkPlugins: [
       remarkObsidian,
