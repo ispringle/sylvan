88a8d12eee1b8eee242adbe4b6f284e501ea9650
Ian S. Pringle <ipringle@jackhenry.com>
2023-01-08 08:02:55 -0600

    new styling, better excerpts

--- src/utils/filterContent.ts
+++ src/utils/filterContent.ts
@@ -1,11 +1,11 @@
-export default function filterContent(content) {
+export default function filterContent(content, filterIndices = true) {
   const devMode = import.meta.env.DEV;
   return devMode
     ? content
-        // .filter((a) => a.frontmatter.title != "index")
+        .filter((a) => !filterIndices || a.frontmatter.kind != "index")
         .sort(sortNewestToOldest)
     : content
-        // .filter((a) => a.frontmatter.title != "index")
+        .filter((a) => !filterIndices || a.frontmatter.kind != "index")
         .filter((a) => a.frontmatter.draft == false)
         .filter((a) => a.frontmatter.public == true)
         .sort(sortNewestToOldest);
