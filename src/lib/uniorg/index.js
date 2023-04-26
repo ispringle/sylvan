010ebbfc94c89f92347ee40bfaa317fe272bf86c
Ian S. Pringle <ipringle@jackhenry.com>
2023-04-26 15:15:15 -0500

    played with and gave up on attachments...maybe l8r

--- src/lib/uniorg/index.js
+++ src/lib/uniorg/index.js
@@ -1,10 +1,12 @@
 import handlers from './handlers.js';
 import { smarty } from './smartypants.ts';
 import { frontmatter } from './frontmatter.js';
+// import { linkAttachments } from './link-attachments.js';
 import { saveRoamRefs } from './roam-refs.js';
 
 export default [
     frontmatter,
+    // linkAttachments,
     saveRoamRefs,
     [smarty, { oldschool: true }],
 ]
---
+++ src/lib/uniorg/link-attachments.js
@@ -0,0 +1,19 @@
+import { visit } from "unist-util-visit";
+
+export const linkAttachments = () => (tree, file) => {
+    visit(tree, "img", (node) => { console.log(node) })
+    visit(tree, "link", (node) => {
+        const isAttachment = node.linkType === "attachment";
+        if (!isAttachment) { return }
+
+        const fileID = file.data.astro.frontmatter?.id
+        if (!fileID) { return }
+
+        const [first, rem] = [fileID.slice(0, 2), fileID.slice(2)]
+        const path = `/.attach/${first}/${rem}/${node.path}`
+        node.linkType = 'file'
+        node.path = path
+        node.rawLink = "file:" + path
+        return node
+    })
+}
