32f9bca2ed27e49f0fe3fcaa27ab90bf9b40e819
Ian S. Pringle <ipringle@jackhenry.com>
2023-08-18 10:48:58 -0500

    j/k rss be broke still

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,17 +2,19 @@ import rss from '@astrojs/rss';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 import { allPages } from "@lib/pages";
 
-export async function get(context) {
-    const pages = await allPages;
-    return rss({
-        title: SITE_TITLE,
-        description: SITE_DESCRIPTION,
-        site: context.site,
-        items: pages.map((page) => ({
-            ...page.data,
-            link: page.slug,
-            title: page.title,
-            pubDate: page?.created || ''
-        })),
-    });
-}
+// export async function get(context) {
+//     const pages = await allPages;
+//     const items = pages.map(({ frontmatter }) => ({
+//         ...frontmatter.data,
+//         link: frontmatter.slug,
+//         title: frontmatter.title,
+//         pubDate: frontmatter?.created || new Date()
+//     }));
+//     console.log(items)
+//     return rss({
+//         title: SITE_TITLE,
+//         description: SITE_DESCRIPTION,
+//         site: context.site,
+//         items: [...items]
+//     });
+// }
