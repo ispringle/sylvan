6527fc084e9d5664151d71385f846ec0d6fa2623
Ian S. Pringle <ipringle@jackhenry.com>
2023-08-23 16:23:31 -0500

    added astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig, sharpImageService } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-
 import org from 'astro-org';
 import icon from "astro-icon";
-//import compress from "astro-compress";
 
 import orgConfig from './src/lib/org-config.js';
 import rehypePlugins from './src/lib/rehype/index.js';
 import remarkPlugins from './src/lib/remark/index.js';
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,22 +16,18 @@ export default defineConfig({
     assets: true
   },
   image: {
-    service: sharpImageService(),
+    service: sharpImageService()
   },
   markdown: {
     rehypePlugins,
     remarkPlugins
   },
-  integrations: [
-    org(orgConfig),
-    icon({
-      include: {
-        "simple-icons": ["*"]
-      }
-    }),
-    sitemap(),
-    // compress()
-  ],
+  integrations: [org(orgConfig), icon({
+    include: {
+      "simple-icons": ["*"]
+    }
+  }), sitemap()
+    , compress()],
   build: {
     inlineStylesheets: 'auto',
     assets: 'assets'
