9f68bda87570eab3d44f7cf754622df982be8884
Ian S. Pringle <ipringle@jackhenry.com>
2023-09-11 15:53:24 -0500

    fixed links, header margin, added prettier

--- src/lib/pages.ts
+++ src/lib/pages.ts
@@ -5,144 +5,144 @@ import { devFilterMaybe, getURL, slugify } from "@utils";
 const PREFER_EXT = "md";
 
 export type Page = {
-	frontmatter: {
-		title: string;
-		id?: string;
-		type?: string;
-		slug: string | undefined;
-		Link: string;
-		canonicalUrl: string;
-		created?: string;
-		modified?: string;
-		link: string;
-		links: string[];
-		tags: string[];
-		draft?: boolean;
-		private?: boolean;
-	};
-	ids: Record<string, string>;
-	file: string;
+  frontmatter: {
+    title: string;
+    id?: string;
+    type?: string;
+    slug: string | undefined;
+    Link: string;
+    canonicalUrl: string;
+    created?: string;
+    modified?: string;
+    link: string;
+    links: string[];
+    tags: string[];
+    draft?: boolean;
+    private?: boolean;
+  };
+  ids: Record<string, string>;
+  file: string;
 };
 const on = <T, R>(value: T, f: (value: T) => R): R => f(value);
 const pages = Object.values(
-	import.meta.glob<true, "", Page>(
-		["../../content/**/*.{md,mdx}", "../../content/**/*.org"],
-		{ eager: true }
-	)
+  import.meta.glob<true, "", Page>(
+    ["../../content/**/*.{md,mdx}", "../../content/**/*.org"],
+    { eager: true }
+  )
 );
 
 export const allPages: Page[] = unique(
-	pages.filter(devFilterMaybe).map(setFrontmatter)
+  pages.filter(devFilterMaybe).map(setFrontmatter)
 );
 
 function setSlug(path: string): string {
-	const isRoot =
-		path.includes("/content/index") || path.includes("/content/site/index");
-	const file = path.split("/content/")[1].split(".")[0];
-	return isRoot ? undefined : slugify(file);
+  const isRoot =
+    path.includes("/content/index") || path.includes("/content/site/index");
+  const file = path.split("/content/")[1].split(".")[0];
+  return isRoot ? undefined : slugify(file);
 }
 
 function setFrontmatter(page: Page): Page {
-	const slug =
-		page.frontmatter.type === "root" ? undefined : setSlug(page.file);
-	page.frontmatter.slug = slug;
-	page.frontmatter.link = slug ? `/${slug}` : "/";
-	page.frontmatter.canonicalUrl = getURL(import.meta.env.SITE, slug);
-	return page;
+  const slug =
+    page.frontmatter.type === "root" ? undefined : setSlug(page.file);
+  page.frontmatter.slug = slug;
+  page.frontmatter.link = slug ? `/${slug}` : "/";
+  page.frontmatter.canonicalUrl = getURL(import.meta.env.SITE, slug);
+  return page;
 }
 
 function unique(pages: Page[]): Page[] {
-	let pageMap = {};
-	pages.forEach((page) => {
-		const newSlug = page.frontmatter.slug || "root";
-		pageMap[newSlug] = pageMap[newSlug]
-			? pageMap[newSlug].file.endsWith(PREFER_EXT)
-				? pageMap[newSlug]
-				: page
-			: page;
-	});
-	return Object.values(pageMap);
+  let pageMap = {};
+  pages.forEach((page) => {
+    const newSlug = page.frontmatter.slug || "root";
+    pageMap[newSlug] = pageMap[newSlug]
+      ? pageMap[newSlug].file.endsWith(PREFER_EXT)
+        ? pageMap[newSlug]
+        : page
+      : page;
+  });
+  return Object.values(pageMap);
 }
 
 const cwd = process.cwd();
 export const resources = Object.fromEntries(
-	Object.entries(
-		import.meta.glob(
-			["../../content/**/*.{png,jpg,jpeg,gif,webp,avif,txt,pdf,sh,mp3,svg}"],
-			{ as: "url" }
-		)
-	).map(([path, loader]) => [
-		fs.realpathSync(join(cwd, "src/lib", path)),
-		loader,
-	])
+  Object.entries(
+    import.meta.glob(
+      ["../../content/**/*.{png,jpg,jpeg,gif,webp,avif,txt,pdf,sh,mp3,svg}"],
+      { as: "url" }
+    )
+  ).map(([path, loader]) => [
+    fs.realpathSync(join(cwd, "src/lib", path)),
+    loader,
+  ])
 );
 
 const idLinks = on(allPages, (posts) => {
-	const ids = new Map();
-	posts.forEach((p) => {
-		const localIds = p.ids;
-		if (localIds) {
-			Object.entries(localIds).forEach(([id, anchor]) => {
-				const link = p.frontmatter.link;
-				ids.set(id, { slug: link, anchor, post: p });
-			});
-		}
-	});
-	return ids;
+  const ids = new Map();
+  posts.forEach((p) => {
+    const localIds = p.ids;
+    if (localIds) {
+      Object.entries(localIds).forEach(([id, anchor]) => {
+        const link = p.frontmatter.link;
+        ids.set(id, { slug: link, anchor, post: p });
+      });
+    }
+  });
+  return ids;
 });
 
 export const resolveId = (id: string) => {
-	const saved = idLinks.get(id);
-	if (!saved) {
-		throw new Error(`Unable to resolve ${id}`);
-	}
+  const saved = idLinks.get(id);
+  if (!saved) {
+    throw new Error(`Unable to resolve ${id}`);
+  }
 
-	const { slug, anchor } = saved;
-	return slug + anchor;
+  const { slug, anchor } = saved;
+  return slug + anchor;
 };
 
 const backlinks = on(allPages, (posts) => {
-	const backlinks: Record<string, Set<Page>> = {};
-	for (const p of posts) {
-		const links = p.frontmatter.links ?? [];
-		for (let link of links) {
-			if (Object.keys(p.ids).includes(link)) {
-				// linking to self -> does not count
-				continue;
-			}
+  const backlinks: Record<string, Set<Page>> = {};
+  for (const p of posts) {
+    const links = p.frontmatter.links ?? [];
+    for (let link of links) {
+      if (Object.keys(p.ids).includes(link)) {
+        // linking to self -> does not count
+        continue;
+      }
 
-			try {
-				link = resolveId(link);
-			} catch {}
+      try {
+        link = resolveId(link);
+      } catch {}
 
-			backlinks[link] = backlinks[link] ?? new Set();
-			backlinks[link].add(p);
-		}
-	}
+      backlinks[link] = backlinks[link] ?? new Set();
+      backlinks[link].add(p);
+    }
+  }
 
-	return backlinks;
+  return backlinks;
 });
 
 export const getBacklinks = (slug: string) => {
-	return backlinks[slug] ?? new Set();
+  return backlinks[slug] ?? new Set();
 };
 
 const getTags = () => {
-	const t = {};
-	allPages.forEach((p) => {
-		const tags = p.frontmatter?.tags || [];
-		tags.forEach(
-			(tag) =>
-				(t[tag] = {
-					color: `hsl(${Math.random() * 360} 100% ${Math.random() * 75 + 25}%)`,
-					pages: [
-						...(t[tag] ? t[tag].pages : []),
-						...[{ frontmatter: p.frontmatter }],
-					],
-				})
-		);
-	});
-	return t;
+  const t = {};
+  allPages.forEach((p) => {
+    const tags = p.frontmatter?.tags || [];
+    tags.forEach(
+      (tag) =>
+        (t[tag] = {
+          color: `hsl(${Math.random() * 360} 100% ${Math.random() * 75 + 25}%)`,
+          pages: [
+            ...(t[tag] ? t[tag].pages : []),
+            ...[{ frontmatter: p.frontmatter }],
+          ],
+        })
+    );
+  });
+  return t;
 };
 
 export const tags = getTags();
