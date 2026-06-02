eea61995bab0f3e2d7491ca2abe33f646e5350c3
Ian S. Pringle <ian@drollery.org>
2026-06-02 11:09:46 -0500

    setup fontes section

--- src/lib/uniorg/handlers.js
+++ src/lib/uniorg/handlers.js
@@ -3,24 +3,26 @@ import { h } from "hastscript";
 export default {
     "verse-block": (org) => {
         const interleave = (a, e) => a.flatMap((x) => [x, e]).slice(0, -1);
-        const verses = org.children.flatMap((n) =>
-            n.type != "text"
-                ? n
-                : interleave(
-                    n.value
-                        .split("\n")
-                        .map((v) => {
-                            if (v != "") {
-                                const value = "\uFEFF" + v.replaceAll(" ", "\u00A0");
-                                return { type: "text", value };
-                            }
-                            return null;
-                        })
-                        .filter((v) => v != null),
-                    h("br")
-                )
-        );
-        return h("p.verse", verses);
+
+        // Split content by double newlines to get stanzas
+        const text = org.children
+            .filter(n => n.type === "text")
+            .map(n => n.value)
+            .join("");
+
+        const stanzas = text.split(/\n\n+/).filter(s => s.trim());
+
+        // Process each stanza
+        const stanzaDivs = stanzas.map(stanza => {
+            const lines = stanza.split("\n").map(line => {
+                const value = "﻿" + line.replaceAll(" ", " ");
+                return { type: "text", value };
+            });
+
+            return h("div.stanza", interleave(lines, h("br")));
+        });
+
+        return h("div.verse", stanzaDivs);
     },
     // superscript parsing is broken in uniorg atm
     // "footnote": (org) => { console.log(org) },
